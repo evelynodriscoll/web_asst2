786694ac2b609849ff9bdc157834ec1b6dd521a0
aramalexanian <123906826+aramalexanian@users.noreply.github.com>
2024-04-09 19:15:35 -0400

    Finished Authentication

--- src/main/java/cst8218/evelyn/bouncer/ApplicationConfig.java
+++ src/main/java/cst8218/evelyn/bouncer/ApplicationConfig.java
@@ -7,23 +7,25 @@ package cst8218.evelyn.bouncer;
 
 import javax.faces.bean.ApplicationScoped;
 import javax.inject.Named;
+import javax.security.enterprise.authentication.mechanism.http.BasicAuthenticationMechanismDefinition;
 import javax.security.enterprise.identitystore.DatabaseIdentityStoreDefinition;
 import javax.security.enterprise.identitystore.PasswordHash;
+import javax.ws.rs.core.Application;
 
 /**
  *
  * @author alexa
  */
 @Named
-//@BasicAuthenticationMechanismDefinition
+@BasicAuthenticationMechanismDefinition
+@ApplicationScoped
 @DatabaseIdentityStoreDefinition(
    dataSourceLookup = "${'java:app/MariaDB'}",
-   callerQuery = "#{'select password from app.appuser where userid = ?'}",
-   groupsQuery = "select groupname from app.appuser where userid = ?",
+   callerQuery = "#{'select password from CST8218.appuser where userid = ?'}",
+   groupsQuery = "select groupname from CST8218.appuser where userid = ?",
    hashAlgorithm = PasswordHash.class,
    priority = 10
 )
-@ApplicationScoped
-public class ApplicationConfig {
+public class ApplicationConfig extends Application {
     
 }
